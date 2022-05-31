5e1fdd6f6c5f209180dfc61bb120af7ffad62217
LucioGhedo <lucioghedina@gmail.com>
2022-05-31 15:19:07 +0200

    dimenticato il colore

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const myName = prompt("Scrivi il tuo nome");
 const myLastname = prompt("Scrivi il tuo cognome");
 const myAge = prompt("Scrivi la tua età");
-const result = myName + myLastname + myAge + '21';
+const myColor = prompt("Ora dimmi il tuo colore preferito");
+const result = myName + myLastname + myAge + myColor + '21';
 console.log(result);
 document.getElementById('result').innerHTML ='La tua nuova password incredibilmente sicura è: ' + result;
